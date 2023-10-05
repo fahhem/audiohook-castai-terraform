0647d7cc662048c1e0a7650608d97fff271d1334
Fahrzin Hemmati <fahhem@gmail.com>
2023-10-05 12:33:20 -0700

    remove eks cluster from castai.tf too

--- castai.tf
+++ castai.tf
@@ -51,148 +51,148 @@ resource "castai_eks_clusterid" "cluster_id" {
   cluster_name = var.cluster_name
 }
 
-module "castai-eks-cluster" {
-  source = "castai/eks-cluster/castai"
-
-  api_url = var.castai_api_url
-  castai_api_token       = var.castai_api_token
-  wait_for_cluster_ready = true
-
-  aws_account_id     = data.aws_caller_identity.current.account_id
-  aws_cluster_region = var.cluster_region
-  aws_cluster_name   = module.eks.cluster_name
-
-  aws_assume_role_arn        = module.castai-eks-role-iam.role_arn
-  delete_nodes_on_disconnect = var.delete_nodes_on_disconnect
-
-  default_node_configuration = module.castai-eks-cluster.castai_node_configurations["default"]
-
-  node_configurations = {
-    default = {
-      subnets = module.vpc.private_subnets
-      tags    = var.tags
-      security_groups = [
-        module.eks.cluster_security_group_id,
-        module.eks.node_security_group_id,
-        aws_security_group.additional.id,
-      ]
-      instance_profile_arn = module.castai-eks-role-iam.instance_profile_arn
-    }
-
-    test_node_config = {
-      subnets = module.vpc.private_subnets
-      tags    = var.tags
-      security_groups = [
-        module.eks.cluster_security_group_id,
-        module.eks.node_security_group_id,
-        aws_security_group.additional.id,
-      ]
-      instance_profile_arn = module.castai-eks-role-iam.instance_profile_arn
-      kubelet_config = jsonencode({
-        "registryBurst" : 20,
-        "registryPullQPS" : 10
-      })
-      container_runtime = "containerd"
-      volume_type       = "gp3"
-      volume_iops       = 3100
-      volume_throughput = 130
-      imds_v1           = true
-    }
-  }
-
-  node_templates = {
-    default_by_castai = {
-      name = "default-by-castai"
-      configuration_id = module.castai-eks-cluster.castai_node_configurations["default"]
-      is_default   = true
-      should_taint = false
-
-      constraints = {
-        on_demand          = true
-        spot               = true
-        use_spot_fallbacks = true
-
-        enable_spot_diversity                       = false
-        spot_diversity_price_increase_limit_percent = 20
-
-        spot_interruption_predictions_enabled = true
-        spot_interruption_predictions_type = "aws-rebalance-recommendations"
-      }
-    }
-    spot_tmpl = {
-      configuration_id = module.castai-eks-cluster.castai_node_configurations["default"]
-      should_taint     = true
-
-      custom_labels = {
-        custom-label-key-1 = "custom-label-value-1"
-        custom-label-key-2 = "custom-label-value-2"
-      }
-
-      custom_taints = [
-        {
-          key = "custom-taint-key-1"
-          value = "custom-taint-value-1"
-        },
-        {
-          key = "custom-taint-key-2"
-          value = "custom-taint-value-2"
-        }
-      ]
-
-      constraints = {
-        fallback_restore_rate_seconds = 1800
-        spot                          = true
-        use_spot_fallbacks            = true
-        min_cpu                       = 4
-        max_cpu                       = 100
-        instance_families = {
-          exclude = ["m5"]
-        }
-        compute_optimized = false
-        storage_optimized = false
-        is_gpu_only = false
-      }
-    }
-  }
-
-  # Configure Autoscaler policies as per API specification https://api.cast.ai/v1/spec/#/PoliciesAPI/PoliciesAPIUpsertClusterPolicies.
-  # Here:
-  #  - unschedulablePods - Unscheduled pods policy
-  #  - nodeDownscaler    - Node deletion policy
-  autoscaler_policies_json = <<-EOT
-    {
-        "enabled": true,
-        "unschedulablePods": {
-            "enabled": true
-        },
-        "nodeDownscaler": {
-            "enabled": true,
-            "emptyNodes": {
-                "enabled": true
-            },
-            "evictor": {
-                "aggressiveMode": false,
-                "cycleInterval": "5m10s",
-                "dryRun": false,
-                "enabled": true,
-                "nodeGracePeriodMinutes": 10,
-                "scopedMode": false
-            }
-        },
-        "clusterLimits": {
-            "cpu": {
-                "maxCores": 20,
-                "minCores": 1
-            },
-            "enabled": true
-        }
-    }
-  EOT
-
-  # depends_on helps Terraform with creating proper dependencies graph in case of resource creation and in this case destroy.
-  # module "castai-eks-cluster" has to be destroyed before module "castai-eks-role-iam".
-  depends_on = [module.castai-eks-role-iam]
-}
+# module "castai-eks-cluster" {
+#   source = "castai/eks-cluster/castai"
+# 
+#   api_url = var.castai_api_url
+#   castai_api_token       = var.castai_api_token
+#   wait_for_cluster_ready = true
+# 
+#   aws_account_id     = data.aws_caller_identity.current.account_id
+#   aws_cluster_region = var.cluster_region
+#   aws_cluster_name   = module.eks.cluster_name
+# 
+#   aws_assume_role_arn        = module.castai-eks-role-iam.role_arn
+#   delete_nodes_on_disconnect = var.delete_nodes_on_disconnect
+# 
+#   default_node_configuration = module.castai-eks-cluster.castai_node_configurations["default"]
+# 
+#   node_configurations = {
+#     default = {
+#       subnets = module.vpc.private_subnets
+#       tags    = var.tags
+#       security_groups = [
+#         module.eks.cluster_security_group_id,
+#         module.eks.node_security_group_id,
+#         aws_security_group.additional.id,
+#       ]
+#       instance_profile_arn = module.castai-eks-role-iam.instance_profile_arn
+#     }
+# 
+#     test_node_config = {
+#       subnets = module.vpc.private_subnets
+#       tags    = var.tags
+#       security_groups = [
+#         module.eks.cluster_security_group_id,
+#         module.eks.node_security_group_id,
+#         aws_security_group.additional.id,
+#       ]
+#       instance_profile_arn = module.castai-eks-role-iam.instance_profile_arn
+#       kubelet_config = jsonencode({
+#         "registryBurst" : 20,
+#         "registryPullQPS" : 10
+#       })
+#       container_runtime = "containerd"
+#       volume_type       = "gp3"
+#       volume_iops       = 3100
+#       volume_throughput = 130
+#       imds_v1           = true
+#     }
+#   }
+# 
+#   node_templates = {
+#     default_by_castai = {
+#       name = "default-by-castai"
+#       configuration_id = module.castai-eks-cluster.castai_node_configurations["default"]
+#       is_default   = true
+#       should_taint = false
+# 
+#       constraints = {
+#         on_demand          = true
+#         spot               = true
+#         use_spot_fallbacks = true
+# 
+#         enable_spot_diversity                       = false
+#         spot_diversity_price_increase_limit_percent = 20
+# 
+#         spot_interruption_predictions_enabled = true
+#         spot_interruption_predictions_type = "aws-rebalance-recommendations"
+#       }
+#     }
+#     spot_tmpl = {
+#       configuration_id = module.castai-eks-cluster.castai_node_configurations["default"]
+#       should_taint     = true
+# 
+#       custom_labels = {
+#         custom-label-key-1 = "custom-label-value-1"
+#         custom-label-key-2 = "custom-label-value-2"
+#       }
+# 
+#       custom_taints = [
+#         {
+#           key = "custom-taint-key-1"
+#           value = "custom-taint-value-1"
+#         },
+#         {
+#           key = "custom-taint-key-2"
+#           value = "custom-taint-value-2"
+#         }
+#       ]
+# 
+#       constraints = {
+#         fallback_restore_rate_seconds = 1800
+#         spot                          = true
+#         use_spot_fallbacks            = true
+#         min_cpu                       = 4
+#         max_cpu                       = 100
+#         instance_families = {
+#           exclude = ["m5"]
+#         }
+#         compute_optimized = false
+#         storage_optimized = false
+#         is_gpu_only = false
+#       }
+#     }
+#   }
+# 
+#   # Configure Autoscaler policies as per API specification https://api.cast.ai/v1/spec/#/PoliciesAPI/PoliciesAPIUpsertClusterPolicies.
+#   # Here:
+#   #  - unschedulablePods - Unscheduled pods policy
+#   #  - nodeDownscaler    - Node deletion policy
+#   autoscaler_policies_json = <<-EOT
+#     {
+#         "enabled": true,
+#         "unschedulablePods": {
+#             "enabled": true
+#         },
+#         "nodeDownscaler": {
+#             "enabled": true,
+#             "emptyNodes": {
+#                 "enabled": true
+#             },
+#             "evictor": {
+#                 "aggressiveMode": false,
+#                 "cycleInterval": "5m10s",
+#                 "dryRun": false,
+#                 "enabled": true,
+#                 "nodeGracePeriodMinutes": 10,
+#                 "scopedMode": false
+#             }
+#         },
+#         "clusterLimits": {
+#             "cpu": {
+#                 "maxCores": 20,
+#                 "minCores": 1
+#             },
+#             "enabled": true
+#         }
+#     }
+#   EOT
+# 
+#   # depends_on helps Terraform with creating proper dependencies graph in case of resource creation and in this case destroy.
+#   # module "castai-eks-cluster" has to be destroyed before module "castai-eks-role-iam".
+#   depends_on = [module.castai-eks-role-iam]
+# }
 
 resource "castai_rebalancing_schedule" "spots" {
 	name = "rebalance spots at every 30th minute"
