0d49fdb5275969da65f81fe6eab5772eac2df0f3
Fahrzin Hemmati <fahhem@gmail.com>
2023-10-12 16:54:36 -0700

    Try another array indexing mechanism?

--- providers.tf
+++ providers.tf
@@ -38,7 +38,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = local.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(local.eks_cluster.certificate_authority[0].data)
+    cluster_ca_certificate = base64decode(local.eks_cluster.certificate_authority.0.data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
