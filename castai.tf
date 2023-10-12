94225c656c12baf3a0fffe1ecf3d99a8c6cc3eef
Fahrzin Hemmati <fahhem@gmail.com>
2023-10-11 19:17:18 -0700

    Remove node_configurations, cleanup providers/data into providers.tf

--- castai.tf
+++ castai.tf
@@ -1,33 +1,8 @@
-# 3. Connect EKS cluster to CAST AI.
-
-locals {
-  role_name   = "castai-eks-role"
-  eks_cluster = data.aws_eks_cluster.existing_cluster
-}
-
 # Configure Data sources and providers required for CAST AI connection.
 data "aws_caller_identity" "current" {}
 
-# Import the existing cluster
-data "aws_eks_cluster" "existing_cluster" {
-  name = var.cluster_name
-}
-
 resource "castai_eks_user_arn" "castai_user_arn" {
   cluster_id = castai_eks_clusterid.cluster_id.id
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = eks_cluster.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed.
-      args = ["eks", "get-token", "--cluster-name", eks_cluster.cluster_name, "--region", eks_cluster.cluster_region]
-    }
-  }
 }
 
 # Create AWS IAM policies and a user to connect to CAST AI.
@@ -35,9 +10,9 @@ module "castai-eks-role-iam" {
   source = "castai/eks-role-iam/castai"
 
   aws_account_id     = data.aws_caller_identity.current.account_id
-  aws_cluster_region = eks_cluster.cluster_region
-  aws_cluster_name   = eks_cluster.cluster_name
-  aws_cluster_vpc_id = eks_cluster.cluster_security_group_id
+  aws_cluster_region = local.eks_cluster.cluster_region
+  aws_cluster_name   = local.eks_cluster.cluster_name
+  aws_cluster_vpc_id = local.eks_cluster.cluster_security_group_id
 
   castai_user_arn = castai_eks_user_arn.castai_user_arn.arn
 
@@ -47,8 +22,8 @@ module "castai-eks-role-iam" {
 # Configure EKS cluster connection using CAST AI eks-cluster module.
 resource "castai_eks_clusterid" "cluster_id" {
   account_id   = data.aws_caller_identity.current.account_id
-  region       = var.cluster_region
-  cluster_name = var.cluster_name
+  region       = local.eks_cluster.cluster_region
+  cluster_name = local.eks_cluster.cluster_name
 }
 
 module "castai-eks-cluster" {
@@ -59,101 +34,11 @@ module "castai-eks-cluster" {
   wait_for_cluster_ready = true
 
   aws_account_id     = data.aws_caller_identity.current.account_id
-  aws_cluster_region = var.cluster_region
-  aws_cluster_name   = var.cluster_name
+  aws_cluster_region = local.eks_cluster.cluster_region
+  aws_cluster_name   = local.eks_cluster.cluster_name
 
   aws_assume_role_arn        = module.castai-eks-role-iam.role_arn
   delete_nodes_on_disconnect = var.delete_nodes_on_disconnect
-
-  default_node_configuration = module.castai-eks-cluster.castai_node_configurations["default"]
-
-  node_configurations = {
-    default = {
-      subnets = module.vpc.private_subnets
-      tags    = var.tags
-      security_groups = [
-        eks_cluster.cluster_security_group_id,
-        eks_cluster.node_security_group_id,
-        aws_security_group.additional.id,
-      ]
-      instance_profile_arn = module.castai-eks-role-iam.instance_profile_arn
-    }
-
-    # test_node_config = {
-    #   subnets = module.vpc.private_subnets
-    #   tags    = var.tags
-    #   security_groups = [
-    #     module.eks.cluster_security_group_id,
-    #     module.eks.node_security_group_id,
-    #     aws_security_group.additional.id,
-    #   ]
-    #   instance_profile_arn = module.castai-eks-role-iam.instance_profile_arn
-    #   kubelet_config = jsonencode({
-    #     "registryBurst" : 20,
-    #     "registryPullQPS" : 10
-    #   })
-    #   container_runtime = "containerd"
-    #   volume_type       = "gp3"
-    #   volume_iops       = 3100
-    #   volume_throughput = 130
-    #   imds_v1           = true
-    # }
-  }
-
-  # node_templates = {
-  #   default_by_castai = {
-  #     name = "default-by-castai"
-  #     configuration_id = module.castai-eks-cluster.castai_node_configurations["default"]
-  #     is_default   = true
-  #     should_taint = false
-
-  #     constraints = {
-  #       on_demand          = true
-  #       spot               = true
-  #       use_spot_fallbacks = true
-
-  #       enable_spot_diversity                       = false
-  #       spot_diversity_price_increase_limit_percent = 20
-
-  #       spot_interruption_predictions_enabled = true
-  #       spot_interruption_predictions_type = "aws-rebalance-recommendations"
-  #     }
-  #   }
-  #   spot_tmpl = {
-  #     configuration_id = module.castai-eks-cluster.castai_node_configurations["default"]
-  #     should_taint     = true
-
-  #     # custom_labels = {
-  #     #   custom-label-key-1 = "custom-label-value-1"
-  #     #   custom-label-key-2 = "custom-label-value-2"
-  #     # }
-
-  #     # custom_taints = [
-  #     #   {
-  #     #     key = "custom-taint-key-1"
-  #     #     value = "custom-taint-value-1"
-  #     #   },
-  #     #   {
-  #     #     key = "custom-taint-key-2"
-  #     #     value = "custom-taint-value-2"
-  #     #   }
-  #     # ]
-
-  #     constraints = {
-  #       fallback_restore_rate_seconds = 1800
-  #       spot                          = true
-  #       use_spot_fallbacks            = true
-  #       min_cpu                       = 4
-  #       max_cpu                       = 100
-  #       instance_families = {
-  #         exclude = ["m5"]
-  #       }
-  #       compute_optimized = false
-  #       storage_optimized = false
-  #       is_gpu_only = false
-  #     }
-  #   }
-  # }
 
   # Configure Autoscaler policies as per API specification https://api.cast.ai/v1/spec/#/PoliciesAPI/PoliciesAPIUpsertClusterPolicies.
   # Here:
--- providers.tf
+++ providers.tf
@@ -1,20 +1,44 @@
 # Following providers required by EKS and VPC modules.
+# 'aws' is provided by Zeet for us
 # provider "aws" {
 #   region = var.cluster_region
 # }
+
+locals {
+  eks_cluster = data.aws_eks_cluster.existing_cluster
+}
 
 provider "castai" {
   api_url   = var.castai_api_url
   api_token = var.castai_api_token
 }
 
+# Import the existing cluster
+data "aws_eks_cluster" "existing_cluster" {
+  name = var.cluster_name
+}
+
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = local.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(local.eks_cluster.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.cluster_region]
+    args = ["eks", "get-token", "--cluster-name", local.eks_cluster.cluster_name, "--region", local.eks_cluster.cluster_region]
   }
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = local.eks_cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(local.eks_cluster.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed.
+      args = ["eks", "get-token", "--cluster-name", local.eks_cluster.cluster_name, "--region", local.eks_cluster.cluster_region]
+    }
+  }
+}
+
