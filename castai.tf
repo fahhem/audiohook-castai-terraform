08fec35361779c11a53b9fe0a12aac5951287991
Fahrzin Hemmati <fahhem@users.noreply.github.com>
2023-11-22 18:08:58 -0800

    Undo changes, they made no difference

--- castai.tf
+++ castai.tf
@@ -81,8 +81,7 @@ module "castai-eks-cluster" {
       tags    = var.tags
       security_groups = concat([
         local.eks_cluster.vpc_config[0].cluster_security_group_id,
-      ], flatten([for vpc_config in local.eks_cluster.vpc_config : tolist(vpc_config.security_group_ids)]))
-      # ], tolist(local.eks_cluster.vpc_config[0].security_group_ids))
+      ], tolist(local.eks_cluster.vpc_config[0].security_group_ids))
       instance_profile_arn = module.castai-eks-role-iam.instance_profile_arn
     }
   }
